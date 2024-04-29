2b7391830af6a71dde4ccf1ca1cfc2a680557a67
Dzhansu <167971328+DzhansuMyumyun@users.noreply.github.com>
2024-04-29 08:08:59 +0200

    created the database

---
+++ RentACar/Persistence/Migrations/20240429060538_Init.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SomeFeatureEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SomeFeatureEntities", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "SomeFeatureEntities",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "BMW" },
+                    { 2, "Mercedes" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SomeFeatureEntities");
+        }
+    }
+}
